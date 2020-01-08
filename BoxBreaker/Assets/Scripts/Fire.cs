015839638ff4afb1ce4a0ab83199253a1cd91e0b
Luke <lukejacobsen11@gmail.com>
2020-01-08 17:35:27 +1000

    Project finished

--- BoxBreaker/Assets/Scripts/Fire.cs
+++ BoxBreaker/Assets/Scripts/Fire.cs
@@ -8,14 +8,37 @@ public class Fire : MonoBehaviour
     public float force;
     public GameObject firePoint;
     GameObject jerry;
+    public bool canFire = false;
+    public GameObject text;
+    public GameObject cannon;
+    public AudioClip shot;
+    AudioSource sound;
 
+    private void Start()
+    {
+        sound = cannon.GetComponent<AudioSource>();
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && canFire)
         {
             jerry = Instantiate(ammo, firePoint.transform.position, Quaternion.identity);
+            sound.PlayOneShot(shot, 1);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        canFire = true;
+        text.SetActive(true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        canFire = false;
+        text.SetActive(false);
+
     }
 }
